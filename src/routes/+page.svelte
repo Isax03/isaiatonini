422e8ae630c8b250f4cc79c0af45e6b43040c12a
Isaia Tonini <isaia.tonini@gmail.com>
2024-04-09 16:54:18 +0200

    added home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 </script>
 
-<section class="flex flex-col w-full items-center justify-start px-5 py-10 text-xl text-primary-foreground  flex-grow">
-    Home
+<section class="flex flex-col w-full items-center justify-start px-8 sm:px-16 py-10 text-xl text-primary-foreground flex-grow">
+    <h2 class="text-4xl font-bold text-center text-secondary-foreground">Hi! <br/> <span class="text-primary-foreground text-3xl *:font font-semibold">Welcome to my website</span></h2>
+    <div class="sm:mt-6 mt-8 flex flex-col">
+        <span>Let me introduce to you:</span>
+        <span class="mt-6 sm:mt-4">My name is <span class="text-secondary-foreground">Isaia</span> and I am a software developer (as you can see, not a designer😅).</span>
+        <span class="mt-5 sm:mt-3">I am currently studying <span class="text-secondary-foreground">Computer Science</span> at the University of Trento.</span>
+        <span class="mt-5 sm:mt-3">I've recently tried my hand at <span class="text-secondary-foreground">web development</span> and I'm starting to get interested in <span class="text-secondary-foreground">mobile development</span>.</span>
+        <span class="mt-5 sm:mt-3">My experience is still limited, and the projects are few, but if you want to take a look, <a href="./projects" class="text-secondary-foreground">click here</a>.</span>
+    </div>
 </section>
