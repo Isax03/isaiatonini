1531b98cf09fb9425b05ae32e1b634d9754e809a
Isaia Tonini <isaia.tonini@gmail.com>
2024-04-11 22:42:31 +0200

    added footer

---
+++ src/lib/components/footer/Footer.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+</script>
+<footer class="bg-zinc-800 text-primary-foreground p-5 w-full flex justify-center">
+	<div class="flex flex-col items-center gap-2">
+		<p>Copyright © 2024 - Isaia Tonini</p>
+	</div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+    import Footer from "$lib/components/footer/Footer.svelte";
     import Navbar from "$lib/components/navbar/Navbar.svelte";
 	import '../app.pcss';
 </script>
 
-<main class="min-h-[100svh]">
+<main class="min-h-[100svh] flex flex-col">
     <Navbar />
-    <slot />
+    <slot/>
 </main>
+<Footer />
