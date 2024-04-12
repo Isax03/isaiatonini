405d31a57b00db0c777139164a1562f8d08486c1
Isaia Tonini <isaia.tonini@gmail.com>
2024-04-12 16:36:26 +0200

    modified style

--- src/lib/components/projects/WipBadge.svelte
+++ src/lib/components/projects/WipBadge.svelte
@@ -2,7 +2,7 @@
 	import Load from 'svelte-material-icons/Loading.svelte';
 </script>
 
-<div class="flex justify-around items-center gap-2 py-1 px-2 rounded-xl bg-secondary-foreground text-zinc-900">
+<div class="flex w-max items-center gap-2 py-1 px-2 rounded-full bg-secondary-foreground text-zinc-900">
 	<span class="text-xs font-semibold">WIP</span>
 	<Load class="w-4 h-4 animate-spin" />
 </div>
